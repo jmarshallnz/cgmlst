d0d99393fa3607ca3a46ad7af3d98b7985695f4d
Jonathan Marshall <jmarshall@never.you.mind>
2024-05-16 15:26:41 +1200

    update download_and_align_pubmlst for caged data

--- download_and_align_pubmlst.R
+++ download_and_align_pubmlst.R
@@ -3,28 +3,33 @@ library(tidyverse)
 library(httr)
 library(seqinr)
 
+out_dir <- 'caged/pubmlst'
+
 # Downloads alleles from pubmlst, sleeping for 20 seconds beteen each one
 download_allele <- function(locus, output_dir, sleep_between = 20) {
   # base URL for pubmlst
-  base_url <- "https://pubmlst.org/bigsdb?db=pubmlst_campylobacter_seqdef&page=downloadAlleles"
   
+  cat("Downloading locus ", locus, "\n")
+  base_url <-
+    paste0("https://rest.pubmlst.org/db/pubmlst_campylobacter_seqdef/loci/", locus, "/alleles_fasta")
+
   # filename to write
   file_name <-  file.path(output_dir, paste0(locus, ".fas"))
   
-  # parse the URL and add the locus to download
-  parsed_url <- parse_url(base_url)
-  parsed_url$query[['locus']] <- locus
-
   # download the file
-  download.file(build_url(parsed_url), file_name)
+  got <- httr::GET(url = base_url)
   
+  content(got) |> write_file(file_name)
+
   # sleep to play nice
   Sys.sleep(sleep_between)
 }
 
 # create output folders
-fs::dir_create('raw')
-fs::dir_create('aligned')
+out_raw <- file.path(out_dir, 'raw')
+out_aligned <- file.path(out_dir, 'aligned')
+fs::dir_create(out_raw)
+fs::dir_create(out_aligned)
 
 # load in the genes
 genes <- read_csv("cgmlst_genes.csv") %>%
@@ -32,12 +37,12 @@ genes <- read_csv("cgmlst_genes.csv") %>%
 
 genes <- genes[1:2]
 
-map(genes, download_allele, sleep_between = 0, output_dir="raw")
+map(genes, download_allele, sleep_between = 0, output_dir=out_raw)
 
 # ok, now align the genes
-fasta_files <- list.files('raw', "*.fas", full.names = TRUE)
+fasta_files <- list.files(out_raw, "*.fas", full.names = TRUE)
 
-align_gene <- function(input_fasta, output_dir, threads=1, direction='none') {
+align_gene <- function(input_fasta, output_dir, threads=1, direction='adjust') {
   extras <- ""
   if (direction == "adjust") {
     extras <- "--adjustdirection"
@@ -56,4 +61,4 @@ align_gene <- function(input_fasta, output_dir, threads=1, direction='none') {
                 output_fasta))
 }
 
-map(fasta_files, align_gene, output_dir="aligned", threads=8)
+map(fasta_files, align_gene, output_dir=out_aligned, threads=8)
